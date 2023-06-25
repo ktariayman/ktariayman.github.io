ca0b5e8bce7d3464cb3a7eb10dea55c4f6757977
Aymanktari <aymanktari31@gmail.com>
2023-06-25 21:53:56 +0100

    add projects

---
+++ script.js
@@ -0,0 +1,292 @@
+const projectsFront = [
+
+    {
+		name: 'Tesla Clone',
+        imageURL :"https://tesla-cdn.thron.com/delivery/public/image/tesla/195458a0-ff67-488c-b972-14d23d2c42fb/bvlatuR/std/1200x630/ms-homepage-social",
+        liveDemo :"https://tesla-clone-6773d.web.app/",
+        github : "https://github.com/ktariayman/tesla_clone"
+	},
+    {
+		name: 'Google clone',
+        imageURL :"https://raw.githubusercontent.com/aashishpanthi/google-clone/master/images/index.png",
+        liveDemo :"https://github.com/ktariayman/Google-Clone-",
+        github : "https://github.com/ktariayman/Google-Clone-"
+	},
+    {
+		name: 'Bondi Design ',
+        imageURL :"https://i.ibb.co/kDM5CS5/bondi-ktariayman-netlify-app.png",
+        liveDemo :"https://bondi-ktariayman.netlify.app/",
+        github : "https://bondi-ktariayman.netlify.app/"
+	}, 
+    {
+		name: 'Food-App',
+        imageURL :"https://i.ibb.co/8dDFwRd/myfoodapp-ktariayman-challenge2-netlify-app.png",
+        liveDemo :"https://myfoodapp-ktariayman-challenge2.netlify.app/",
+        github : "https://github.com/ktariayman/my-food-app"
+	}, 
+    {
+		name: ' Weather-App',
+        imageURL :"https://i.ibb.co/dKms2VB/myweatherapp-ktariayman-challenge4-netlify-app.png",
+        liveDemo :"https://myweatherapp-ktariayman-challenge4.netlify.app/",
+        github : "https://github.com/ktariayman/WeatherApp-"
+	},  
+    {
+		name: 'Shopping-Cart ( react-router-v6)',
+        imageURL :"https://i.ibb.co/Thsjmc7/cartshop-ktariayman-netlify-app-cart.png",
+        liveDemo :"https://cartshop-ktariayman.netlify.app/",
+        github : "https://github.com/ktariayman/Shopping-Cart-ReactJS-"
+	}, 
+    {
+		name: ' Quote-App',
+        imageURL :"https://i.ibb.co/WVvhQ7d/myquoteapp-ktariayman-challenge3-netlify-app.png",
+        liveDemo :"https://myquoteapp-ktariayman-challenge3.netlify.app/",
+        github : "https://github.com/ktariayman/QuoteApp"
+	},
+    {
+		name: 'Password-Generator-App',
+        imageURL :"https://i.ibb.co/CJT8Ggt/passwordgenerator-ktariayman-challenge6-netlify-app.png",
+        liveDemo :"https://passwordgenerator-ktariayman-challenge6.netlify.app/",
+        github : "https://github.com/ktariayman/passwordGenerator"
+	},  
+    {
+		name: 'Pomodoro-Tracker-App',
+        imageURL :"https://i.ibb.co/w65BCQV/mypromodero-tracker-ktariayman-challenge8-netlify-app.png",
+        liveDemo :"https://mypromodero-tracker-ktariayman-challenge8.netlify.app/",
+        github : "https://github.com/ktariayman/promodero-tracker"
+	}, 
+    {
+		name: 'Color Generator',
+        imageURL :"https://i.ibb.co/wY4HNyb/colors-generator-ktariayman-challenge9-netlify-app.png",
+        liveDemo :"https://colors-generator-ktariayman-challenge9.netlify.app/",
+        github : "https://github.com/ktariayman/color-generator"
+	}, 
+    {
+		name: 'Quiz-App',
+        imageURL :"https://i.ibb.co/FKnnm5p/react-o7inu3-stackblitz-io.png",
+        liveDemo :"https://react-o7inu3.stackblitz.io/",
+        github : "https://github.com/ktariayman/Quiz-App"
+	},    
+    {
+		name: 'Shopping Cart ',
+        imageURL :"https://i.ibb.co/Ry3wp9h/myshoppingcart-ktariayman-challenge5-netlify-app.png",
+        github : "https://github.com/ktariayman/Shopping-Cart",
+        liveDemo :"https://myshoppingcart-ktariayman-challenge5.netlify.app/"
+	}, 
+ 
+    {
+		name: 'Text-Editor-App',
+        imageURL :"https://i.ibb.co/rxjkvnz/ktariayman-github-io-text-Editor.png",
+        github :"https://github.com/ktariayman/textEditor",
+        liveDemo : "https://ktariayman.github.io/textEditor/"
+	},
+    {
+		name: 'Beatbox App ',
+        imageURL :"https://i.ibb.co/0yZSgWF/beatbox-ktariayman-netlify-app.png",
+        liveDemo :"https://beatbox-ktariayman.netlify.app/",
+        github : "https://github.com/ktariayman/Beatbox-"
+	},
+    {
+		name: 'Guess the number',
+        imageURL :"https://i.ibb.co/k9shf21/guessthenumber-ktariayman-netlify-app.png",
+        liveDemo :"https://guessthenumber-ktariayman.netlify.app/",
+        github : "https://github.com/ktariayman/guess-the-number"
+	}, 
+    {
+		name: 'Stats preview card component',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/t26y9p3veejvbc9biv3f.jpg",
+        liveDemo :"https://ktariayman.github.io/Stats-preview-card-component-solution/",
+        github : "https://github.com/ktariayman/Stats-preview-card-component-solution"
+	}, 
+    {
+		name: '3-column-preview-card',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/ap7h50kkrdq7zclbokox.jpg",
+        liveDemo : "https://ktariayman.github.io/column-preview-card-component/",
+        github :"https://github.com/ktariayman/column-preview-card-component",
+
+    },
+    {
+		name: 'four-card-feature-section',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/wbsdema37uawkvkp9lab.jpg",
+        liveDemo :"https://ktariayman.github.io/Stats-preview-card-component-solution/",
+        github : "https://github.com/ktariayman/Four-card-feature-section-challenge"
+    },    
+    {
+		name: 'Social proof section        ',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/c5mnesn5eknealmjz4w2.jpg",
+        liveDemo :"https://ktariayman.github.io/Social-proof-section/",
+        github : "https://github.com/ktariayman/Social-proof-section"
+	},
+    {
+		name: 'Qr Code component        ',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/cybxdhr4wewlscvco9dd.jpg",
+        liveDemo :"https://csb-2ww33.netlify.app/",
+        github : "https://github.com/ktariayman/QR-code-component"
+	},   
+    {
+		name: 'NFT preview card component',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/sr9i7z9p1tfb2m40pigu.jpg",
+        liveDemo :"https://keen-murdock-7a2ac4.netlify.app/",
+        github : "https://github.com/ktariayman/NFT-preview-card-component"
+	},
+    {
+		name: 'profile-card-component',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/tsdyz2ccordcztyrn8m0.jpg",
+        liveDemo :"https://ktariayman.github.io/profile-card-component/",
+        github : "https://github.com/ktariayman/profile-card-component"
+	},
+    {
+		name: 'Order-summary-component        ',
+        imageURL :"https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/gh4wbxnbnf9wqezb0b6y.jpg",
+        liveDemo :"https://ktariayman.github.io/Order-summary-component/",
+        github : "https://github.com/ktariayman/Order-summary-component"
+	}, 
+];
+
+const projectsBackend = [
+    {
+		name: 'TodoApp  Api ( MongoDb , Express , Node) ',
+        imageURL :"https://i.ibb.co/1LXnMkP/todoapp-api.png",
+        liveDemo :"https://github.com/ktariayman/toDoApp-MERN-",
+        github : "https://github.com/ktariayman/toDoApp-MERN-"
+
+	},
+    {
+		name: 'Product  Api ( Node, TypeScript , typeorm , Mysql) ',
+        imageURL :"https://i.ibb.co/qdbnrSq/product-Api.png",
+        liveDemo :"https://github.com/ktariayman/Product-API",
+        github : "https://github.com/ktariayman/Product-API"
+
+	},
+    {
+		name: 'Social Media Api ( MongoDb , Express , Node) ',
+        imageURL :"https://i.ibb.co/jgQ71XS/socialmediaapi.png",
+        liveDemo :"https://github.com/ktariayman/Social-media-rest-api",
+        github : "https://github.com/ktariayman/Social-media-rest-api"
+
+	},
+    
+];
+
+const projectsFullStack = [
+    {
+        name: 'fullstack Chat App ( MERN + WEBRTC + SOCKET.IO  ! Working on ) ',
+        imageURL :"https://i.ibb.co/8sMfB11/a01d80e8-6fb3-4dad-beb1-d281dde2f5df.jpg",
+        liveDemo :"https://github.com/ktariayman/chat-app-mern",
+        github : "https://github.com/ktariayman/chat-app-mern"
+    },
+    {
+		name: 'Fullstack E-commerce App ( MERN ) ',
+        imageURL :"https://i.ibb.co/S0n4fvG/download.jpg",
+        liveDemo :"https://github.com/ktariayman/E-commerce-mern",
+        github : "https://github.com/ktariayman/E-commerce-mern"
+
+	},
+    {
+		name: 'fullstack Switchy App ( React / Sanity ) ',
+        imageURL :"https://i.ibb.co/Z1LMW40/switchy-app-netlify-app-1.png",
+        liveDemo :"https://switchy-app.netlify.app/",
+        github : "https://switchy-app.netlify.app/"
+	}, 
+    // {
+	// 	name: 'FullStack Netflix Clone ( React / Firebase ) ',
+    //     imageURL :"https://res.cloudinary.com/practicaldev/image/fetch/s--_i4LYB4J--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/qbsciyoo1m5174v5zcxz.png",
+    //     liveDemo :"https://github.com/ktariayman/Netflix-Clone",
+    //     github : "https://github.com/ktariayman/Netflix-Clone"
+
+	// },
+];
+
+
+const list1 = document.getElementById('list1');
+const list2 = document.getElementById('list2');
+const list3 = document.getElementById('list3');
+
+projectsFront.forEach(({ name, imageURL ,liveDemo,github}, i) => {
+	const listItem = document.createElement('li');
+    console.log(liveDemo)
+	listItem.innerHTML = `
+		<a href="${liveDemo}" target="_blank" >
+			<img src="${imageURL}" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+
+		<div class="links-container">
+
+                <a href="${liveDemo}" target="_blank" class="blue">
+                    <i class="fas fa-eye"></i>
+                </a>
+                <a href="${github}" target="_blank" class="blue">
+                    <i class="fab fa-github"></i>		
+                </a>
+                
+            
+            
+
+			
+		</div>
+	`;
+
+	list1.appendChild(listItem);
+});
+projectsFullStack.forEach(({ name, imageURL ,liveDemo,github}, i) => {
+    const listItem = document.createElement('li');
+    console.log(liveDemo)
+	listItem.innerHTML = `
+    <a href="${liveDemo}" target="_blank" >
+			<img src="${imageURL}" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+            </a>
+
+		<div class="links-container">
+        
+                <a href="${liveDemo}" target="_blank" class="blue">
+                    <i class="fas fa-eye"></i>
+                </a>
+                <a href="${github}" target="_blank" class="blue">
+                    <i class="fab fa-github"></i>		
+                    </a>
+                
+                    
+                    
+                    
+                    
+                    </div>
+                    `;
+
+                    list2.appendChild(listItem);
+});
+
+projectsBackend.forEach(({ name, imageURL ,liveDemo,github}, i) => {
+    const listItem = document.createElement('li');
+    console.log(liveDemo)
+    listItem.innerHTML = `
+        <a href="${liveDemo}" target="_blank" >
+            <img src="${imageURL}" alt="${name}" />
+            <p>${i + 1}. ${formatProjectName(name)}</p>
+        </a>
+
+        <div class="links-container">
+
+                <a href="${liveDemo}" target="_blank" class="blue">
+                    <i class="fas fa-eye"></i>
+                </a>
+                <a href="${github}" target="_blank" class="blue">
+                    <i class="fab fa-github"></i>		
+                </a>
+                
+            
+            
+
+            
+        </div>
+    `;
+
+    list3.appendChild(listItem);
+});
+function formatProjectName(name) {
+    return name
+    .split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
+1
